56bd3c0bff689af932412c2618f101ee4e0d1cd5
Topher Cullen <topher.cullen@jamberry.com>
2015-08-20 17:17:56 +0000

    Remove jamberry specific attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,15 @@
 default['craft']['install']['version']['major'] = '2.4'
 default['craft']['install']['version']['minor'] = '2670'
 default['craft']['install']['url'] = 'http://download.buildwithcraft.com/craft'
-default['craft']['app_home'] = '/opt/jamberry'
+default['craft']['app_home'] = '/opt'
 default['craft']['craft_home'] = "#{node['craft']['app_home']}/craft"
 default['craft']['db']['host'] = 'localhost'
 default['craft']['db']['user'] = 'root'
 default['craft']['db']['password'] = 'changeme'
 default['craft']['db']['database'] = 'craft'
 default['craft']['db']['prefix']  = 'craft'
-default['craft']['ports']['http_port'] = node["craft_data"]["application_layout"]["http_port"]
-default['craft']['ports']['https_port'] = node["craft_data"]["application_layout"]["https_port"]
+default['craft']['ports']['http_port'] = '80'
+default['craft']['ports']['https_port'] = '443'
 default['craft']['cert'] = '/etc/httpd/ssl/craft.pem'
 default['craft']['license'] = true
 
